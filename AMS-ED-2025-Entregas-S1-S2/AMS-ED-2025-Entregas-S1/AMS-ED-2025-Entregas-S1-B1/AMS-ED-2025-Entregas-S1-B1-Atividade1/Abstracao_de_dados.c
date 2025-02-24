c518e9707e2e1ee4654dd8162ca76c1f73e66dce
SamucaMello <samuel.mellomail@gmail.com>
2025-02-23 21:28:28 -0300

    novas_pastas

---
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade1/Abstracao_de_dados.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void aplicarDesc(float* preco, float* prodDesc, float* precoTotal){
+    
+    if(*prodDesc < 0){
+        *prodDesc = 0;
+    }
+    
+    *precoTotal = *preco - ((*preco) * (*prodDesc)  / 100.0);
+}
+
+int main(){
+    char 
+        nomeProd[32];
+    
+    float 
+        preco,
+        prodDesc,
+        precoTotal;
+    
+    float 
+        *precoPtr      = &preco,
+        *prodDescPtr   = &prodDesc,
+        *precoTotalPtr = &precoTotal;
+ 
+    
+    printf("Coloque o nome do produto:\nNome: ");
+    scanf("%s", nomeProd);
+    
+    printf("Coloque o preço do produto:\nR$:");
+    scanf("%f", precoPtr);
+    
+    printf("Coloque o desconto do produto:\n");
+    scanf("%f", prodDescPtr);
+    
+    aplicarDesc(precoPtr, prodDescPtr, precoTotalPtr);
+
+    
+    printf("\n\nResultado:\nNome do produto: %s\nPreço Inicial: %.2fR$\nDesconto: %.2f\nPreço total com desconto: %.2fR$\n",
+        nomeProd, *precoPtr, *prodDescPtr, *precoTotalPtr
+    );
+   
+    return 0;
+}
