ba96e8a86f2d3e3a6d3e65183f1f87487b2c163d
SamucaMello <samuel.mellomail@gmail.com>
2025-04-01 16:26:39 -0300

    cabeçalho

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade3/lista_ligada.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade3/lista_ligada.c
@@ -1,3 +1,12 @@
+/*----------------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados                    */
+/*                         Id da Atividade: S1-B1-3                                 */
+/*             Objetivo: << Lista ligada para restaurante >>                        */
+/*                                                                                  */
+/*                                  Autor: Samuel Mello de Campos e Silva           */
+/*                                                                  Data:25/03/2025 */
+/*----------------------------------------------------------------------------------*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
